eba7b4b1c82a1f8a5785a91b3a9aa7a283465bf6
Cesar Ivan Alvarez <cesarivanalvarezmendoza@gmail.com>
2025-11-27 11:46:00 +0100

    Update UPLOAD_ANAPPTOSHINYWEB.R

--- UPLOAD_ANAPPTOSHINYWEB.R
+++ UPLOAD_ANAPPTOSHINYWEB.R
@@ -1,8 +1,9 @@
 install.packages('rsconnect')
 
-rsconnect::setAccountInfo(name='cesarivanalvarez', token='F31E90D4FAF737354ADF3C313A5D5191', secret='rDyTee5bfg8nyzisJ1qiPA4i7xZo6vqzX9P4gH5b')
+rsconnect::setAccountInfo(name='cesarivanalvarez', token='F31E90D4FAF737354ADF3C313A5D5191', secret='XXXXXXXX') 
 
 library(rsconnect)
-# Here put the folder where is your shiny app. It must be called app.R inside the folder you choose
+# Here put the folder where is your shiny app. It must be called app.R inside the folder you choose - CHANGE SECRET XXX FOR YOUR CREDENTIAL
 rsconnect::deployApp('C:/DWD_Temperature_App') HERE_REPLACE_FOR_SHINY_CREDENTIALS
 rsconnect::showLogs(appName = "DWD_Temperature_Visualizer", streaming = TRUE)
+
