1c2ad1ccc3d1bdac1b07cd8b2072c7d4a5eaba49
Cesar Ivan Alvarez <cesarivanalvarezmendoza@gmail.com>
2025-12-15 10:13:26 +0100

    Delete UPLOAD_ANAPPTOSHINYWEB.R

--- UPLOAD_ANAPPTOSHINYWEB.R
+++
@@ -1,9 +0,0 @@
-install.packages('rsconnect')
-
-library(rsconnect)
-
-rsconnect::setAccountInfo(name='cesarivanalvarez', token='YYYYYYY', secret='XXXXXX') # Here put your credentials get when you create an account on ShinyApps
-
-# Here put the folder where is your shiny app. It must be called app.R inside the folder you choose, additionally the data you used must be in the same folder
-rsconnect::deployApp('C:/EcuadorInamhi')
-rsconnect::showLogs(appName = "Ecuador_Inamhi", streaming = TRUE)
